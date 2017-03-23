e62d4119e52d37147c69936efc4bfa4d6ca363ac
Unknown <audeer2019@mirmanmustangs.org>
2017-03-23 14:06:13 -0700

    Get Mathsteps Working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import {simplifyExpression} from "mathsteps";
+import {simplifyExpression, solveEquation, ChangeTypes} from "mathsteps";
 
-const steps = simplifyExpression('2x + 2x + x + x');
+const steps = solveEquation('2x + 3x = 35');
 
 steps.forEach(step => {
-    console.log("before change: " + step.oldNode);         // before change: 2 x + 2 x + x + x
-    console.log("change: " + step.changeType);             // change: ADD_POLYNOMIAL_TERMS
-    console.log("after change: " + step.newNode);          // after change: 6 x
-    console.log("# of substeps: " + step.substeps.length); // # of substeps: 3
+    console.log("before change: " + step.oldEquation.print());  // e.g. before change: 2x + 3x = 35
+    console.log("change: " + step.changeType);                  // e.g. change: SIMPLIFY_LEFT_SIDE
+    console.log("after change: " + step.newEquation.print());   // e.g. after change: 5x = 35
+    console.log("# of substeps: " + step.substeps.length);      // e.g. # of substeps: 2
 });
