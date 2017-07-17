a93128c5652c738ba6af7ea1df5fc1337ef4dde5
Flashomatic <augustmkraft@gmail.com>
2017-07-17 13:47:58 -0700

    fix port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 const express = require("express");
 const bodyParser = require("body-parser");
-const port = 3000;
+const https = require("https");
 const app = express();
 const hintSolve = require("./src/index.js");
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -20,6 +22,6 @@ app.route("/solve")
     });
 });
 
-app.listen(port, () => {
-    console.log(`Listening on ${port}!`);
-})
+app.listen(app.get('port'), () => {
+    console.log(`Listening on ${app.get('port')}!`);
+});
